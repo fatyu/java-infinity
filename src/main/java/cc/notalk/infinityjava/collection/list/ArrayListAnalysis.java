9edf19c9bd7867960a24d111172fbb80844540ad
fatyu <33862677@qq.com>
2018-04-25 00:04:56 +0800

    :pencil2:添加arraylist代码介绍. 添加ignore配置

--- src/main/java/cc/notalk/infinityjava/collection/list/ArrayListAnalysis.java
+++ src/main/java/cc/notalk/infinityjava/collection/list/ArrayListAnalysis.java
@@ -20,3 +20,16 @@ public class ArrayListAnalysis {
         System.out.println("list data is :" + list.toString());
     }
 }
+
+//----关于arraylist的描述
+/*
+Resizable-array implementation of the <tt>List</tt> interface.  Implements
+ all optional list operations, and permits all elements, including
+ <tt>null</tt>.  In addition to implementing the <tt>List</tt> interface,
+ this class provides methods to manipulate the size of the array that is
+ used internally to store the list.  (This class is roughly equivalent to
+ <tt>Vector</tt>, except that it is unsynchronized.)
+ #翻译如下:
+ arraylist是实现了list接口的可扩容数组,实现了List接口的所有方法,和存储所有元素(包括null)
+ 为了扩展实现list接口,这个类提供了操作数组(这个类是基于数组实现)的很多方法(和arraylist相似的是vector,只不过arraylist不是同步的.)
+ */
