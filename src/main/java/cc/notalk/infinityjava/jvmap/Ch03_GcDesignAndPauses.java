b7944aed155ade4ee76ef46f8a8ec2f60ce460a4
fatyu <33862677@qq.com>
2018-08-21 17:23:00 +0800

    :beers: 添加代码执行配置

--- src/main/java/cc/notalk/infinityjava/jvmap/Ch03_GcDesignAndPauses.java
+++ src/main/java/cc/notalk/infinityjava/jvmap/Ch03_GcDesignAndPauses.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 1.运行G1作为垃圾回收器
- */
+ * 前提：使用最新的jdk9
+ * 1.运行G1作为垃圾回收器  -Xms4G -Xmx4G -Xlog:gc
+ * 2.使用并行回收器 -XX:+UseParallelOldGC -Xms4G -Xmx4G -Xlog:gc
+ * 3.使用CMS -XX:+UseConcMarkSweepGC -Xms4G -Xmx4G -Xlog:gc
+ * 4.使用开发中的Shenandoah -XX:+UseConcMarkSweepGC -Xms4G -Xmx4G -Xlog:gc
+ * 5.使用Epsilon???没看懂! -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -Xms4G -Xmx4G
+ *  无操作GC，会根据当前环境进行判断是否需要做GC，减少gc的时间消耗?
+ * */
 
 public class Ch03_GcDesignAndPauses {
     static List<Object> l;
